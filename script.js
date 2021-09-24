6f89885bb3885b7a2d1f1c9b54cbf06e9e76e553
johang <dampig14@gmail.com>
2021-09-23 22:59:49 -0500

    vamos bien xd

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-alert("Hola mor :3");
+"use strict"
 
 
+var nombre = "Hola mi amor xd";
+
+console.log(nombre);
+
+// alert(nombre);
+// alert("Hola mor :3");
 
 
+function saludo() {
+
+    // let nombre = "Qué mas pues <3";
+    // console.log(nombre);
+
+    let saludo = "I want to be with you ❤️";
+    console.log(saludo);
+}
+
+saludo();
 
 
+var edad = "24.9";
 
+// var suma = Number(edad);
+
+// parseFloat(suma);
+// var insuma = parseFloat(edad);
+
+// console.log(insuma*10);
+
+
+var numero = 50;
+
+console.log(numero);
+
+var numeroTxt = String(numero);
 
 
 
